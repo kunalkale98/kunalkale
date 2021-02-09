1268018275f01c4fd7bf9d857cf85c433946e776
kunalkale98 <kunal,k2898@gmail.com>
2021-02-09 17:33:25 +0530

    Added: Palindrome Function

---
+++ Function-programming/palindromUsingFn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+echo "Enter a number to check its palindrome or not:"
+num=$1
+
+function palindrome() {
+	local num=$1
+	new_num=0
+	while [ $num -ne 0 ]
+	do
+		n=$(($num%10))
+		new_num=$((($new_num*10)+$n))
+		num=$(($num/10))
+	done
+	echo $new_num
+}
+
+outputNo=$( palindrome $num )
+if [ $outputNo -eq $num ]
+	then
+		echo "Number is palindrome"
+else
+	echo "Number is not palindrome"
+fi
+
