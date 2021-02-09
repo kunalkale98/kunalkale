dfdbd406e803bdcd29e6eea0a7ec8cae01d77bad
kunalkale98 <kunal,k2898@gmail.com>
2021-02-09 15:24:29 +0530

    Added Magic Number example

--- While-loop-statement/magicNumber.sh
+++ While-loop-statement/magicNumber.sh
@@ -1,5 +1,31 @@
 #!/bin/bash -x
 
-num=$1
+echo "Enter a number between 1 to 100:"
+
+read a
+
+#k=30
+#i=0
+number=$(((RANDOM%100)+1))
+echo "The number to be guessed is $guess"
+
+while [ $number -ne $a ]
+do
+	if [ $number -gt $a ]
+		then
+			number=$(($number/2))
+	elif [ $number -lt $a ]
+		then
+			new=$(($number/2))
+			number=$(($number+$new))
+	fi
+	((i++))
+done
+
+if [ $number -eq $a ]
+      then
+         echo "The number is $number"
+fi
+
 
 
